9a96e48c1eb3940721be38ce15a93b847854979d
Yudhvir Raj <rajyudhvir29@gmail.com>
2017-11-28 22:32:53 -0800

    Added dialog

---
+++ Preventanyl/app/src/main/java/noverdose/preventanyl/OverdoseDialog.java
@@ -0,0 +1,48 @@
+package noverdose.preventanyl;
+
+/**
+ * Created by yudhvirraj on 2017-11-28.
+ */
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yudhvirraj on 2017-09-07.
+ */
+
+
+public class OverdoseDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Overdose")
+                .setMessage("5")
+                .setCancelable(false)
+                .setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getDialog ().setCanceledOnTouchOutside (false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 ## 97bc4346faf5cfe7c3e87cdba0a31da248221d2d - 2017-11-28 - Yudhvir
 ### Added
+- Dialog class for overdose
+
+## 97bc4346faf5cfe7c3e87cdba0a31da248221d2d - 2017-11-28 - Yudhvir
+### Added
 - Push notifications
 - Firebase Cloud messaging
 
